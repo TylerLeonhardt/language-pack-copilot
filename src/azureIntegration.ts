90efffcd38656e31e1229fb46629983454da3b0d
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2024-06-10 15:49:58 -0700

    Fix and document the Azure integration

--- src/azureIntegration.ts
+++ src/azureIntegration.ts
@@ -2,26 +2,31 @@ import { AzureOpenAI } from "openai";
 import { DefaultAzureCredential, getBearerTokenProvider } from "@azure/identity";
 
 export async function translatePhrases(phrases: string[]): Promise<string | null> {
-  const scope = "https://cognitiveservices.azure.com/.default";
-  const azureADTokenProvider = getBearerTokenProvider(new DefaultAzureCredential(), scope);
-  const deployment = "gpt-35-turbo";
-  const apiVersion = "2024-04-01-preview";
+  // I am currently using the process.env["AZURE_OPENAI_API_KEY"] to hold the API key
+  // instead of using @azure/identity.
+  // const scope = "https://cognitiveservices.azure.com/.default";
+  // const azureADTokenProvider = getBearerTokenProvider(new DefaultAzureCredential(), scope);
+
+  // The name of the deployment in Azure OpenAI that we want to use.
+  const deployment = "Turbo";
   const endpoint = "https://vscode-openai.openai.azure.com/";
-  const client = new AzureOpenAI({ azureADTokenProvider, deployment, apiVersion, endpoint });
+  const apiVersion = "2024-04-01-preview"; // Not sure what are the possible values here.
+  const client = new AzureOpenAI({ deployment, apiVersion, endpoint });
   const result = await client.chat.completions.create({
     messages:  [
       { role: "system", content: "You are a translator. The phrases you translate will be related to Visual Studio Code, a code editor. Your goal is to output a sentence in English given the same sentence in multiple languages. Pick a concise translation that best captures the meaning of the original sentence." },
-      { role: "user", content: "Закрыть диалоговое окно, Zavřít dialogové okno, Cerrar cuadro de diálogo" },
-      { role: "assistant", content: "Close Dialog" },
+      { role: "user", content: "Закрыть диалоговое окно, Zavřít dialogové okno, Cerrar cuadro de diálogo" }, // example 1 user input
+      { role: "assistant", content: "Close Dialog" }, // example 1 bot response
       { role: "user", content: phrases.join(", ") },
     ],
     model: deployment,
     top_p: 0.95,
   });
 
-  for (const choice of result.choices) {
-    console.log(choice.message);
-  }
+  // Example output: {content: 'Use Regular Expression', role: 'assistant'}
+  // for (const choice of result.choices) {
+  //   console.log(choice.message);
+  // }
 
   return result.choices[0].message.content;
 }
