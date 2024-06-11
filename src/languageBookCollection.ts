5989833450be3493ee6c9f130d1a492c071fa06b
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2024-06-11 09:49:23 -0700

    Add more typings

--- src/languageBookCollection.ts
+++ src/languageBookCollection.ts
@@ -7,8 +7,16 @@ import { getTranslationsStrings } from "./utils/getTranslationsStrings";
 export type LanguageName = string;
 export type ILanguageBookCollection = Record<LanguageName, ILanguageBook>;
 
+// e.g. "vs/base/browser/ui/actionbar/actionViewItems"
+export type LanguagePackChunkCategory = string;
+// e.g. { "button dropdown more actions": "Další akce..." }
+export type LanguagePackChunk = Record<string, string>;
+export type LanguagePackFile = {
+    version: string;
+    contents: Record<LanguagePackChunkCategory, LanguagePackChunk>;
+};
 export type FileName = string;
-export type ILanguageBook = Record<FileName, any>;
+export type ILanguageBook = Record<FileName, LanguagePackFile>;
 
 export async function getLanguageBookCollection(): Promise<ILanguageBookCollection> {
     const languagePacks = await fetchLanguagePacks();
